d64523ca1cb63548edca4c6ed2a4deb0c77a78c1
Fetiya Yusuf <152329383+fafiyusuf@users.noreply.github.com>
2025-07-15 22:06:32 +0300

    Add solution for Sorting the Sentence

---
+++ LeetCode/Sorting-the-Sentence/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        def isnumber(word):
+            number=''
+            for char in word:
+                if char.isdigit():
+                    number += char
+            return int(number)
+        array = s.split()
+        sorted_array = sorted(array , key=isnumber)
+        result = []
+        for i in sorted_array:
+            x = i[:-1]
+            result.append(x)
+        result = ' '.join(result)
+        return result
+
+        # array = sorted(array)
+        
